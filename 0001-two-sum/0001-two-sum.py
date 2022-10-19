0f7cfa23efbf9deafabb15ae791f244f404d350d
Shopiley <82163647+Shopiley@users.noreply.github.com>
2022-10-19 13:27:01 +0100

    Time: 1320 ms (28.52%), Space: 14.9 MB (78.76%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #create a hash map
+        #iterate through the array
+        #check the value r for which target - values in our hash map (false cause hashmap is empty the first time): return index of r in hash map
+        #store values:index in the hash map 
+        
+        for i in range(len(nums)):
+            r = target - nums[i]
+            if r in nums:
+                if nums.index(r) != i:
+                    return (i, nums.index(r))
+        
+        
+        
+        
+        
+        
+        # for i in range(0, len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         # if j <= i:
+        #         #     continue
+        #         if (nums[j] == target - nums[i]):
+        #             return (i, j)
+        
