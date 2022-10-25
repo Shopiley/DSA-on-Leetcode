7ab75489d3347f4886df4c8b5ebf37440dd57350
Shopiley <82163647+Shopiley@users.noreply.github.com>
2022-10-25 18:36:02 +0100

    Time: 176 ms (68.75%), Space: 67.7 MB (53.67%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxCount=INT_MIN, temp=0;
+        for(int i=0; i<nums.size(); i++){
+            temp+=nums[i];
+            if(temp <nums[i]) temp = nums[i];
+            if(maxCount<temp) maxCount = temp; 
+        }
+        return maxCount;
+    }
+};
+// comparing sum with current element. 
+// if smaller than current element then leave all the previous counts.
