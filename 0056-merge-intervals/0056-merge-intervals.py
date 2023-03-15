534fb88305389bcf8920bdc0bbd89b8474534bdb
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-03-15 12:55:55 +0100

    Time: 169 ms (15.72%), Space: 18.2 MB (45.99%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,35 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        """
+        1236
+        intervals = [[1,4],[2,3],[8,10],[15,18]]
+        1 2 3 4
+        
+        1 < 2 <= 3
+        
+        
+        
+        """
+        intervals.sort()
+        i = 0
+        while i < len(intervals)-1:
+            max_start = max(intervals[i][0], intervals[i+1][0])
+            min_end = min(intervals[i][1], intervals[i+1][1])
+            
+            if max_start <= min_end:                
+                intervals[i+1] = [min(intervals[i][0], intervals[i+1][0]), max(intervals[i][1], intervals[i+1][1])]
+                del intervals[i]
+                # print(intervals)
+                # merged.append()
+                
+            else:
+                # merged.append(intervals[i])
+                i += 1
+        
+        # if i < len(intervals):
+        #     merged.append(intervals[i])
+        return intervals
+                
+            
+         
