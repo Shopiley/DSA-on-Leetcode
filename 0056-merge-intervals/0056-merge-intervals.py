861a72ac7344e06382c407680f9abe9fc6ff177f
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-03-15 12:56:22 +0100

    Time: 180 ms (11.77%), Space: 18.1 MB (76.90%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,9 +6,7 @@ class Solution:
         intervals = [[1,4],[2,3],[8,10],[15,18]]
         1 2 3 4
         
-        1 < 2 <= 3
-        
-        
+        1 < 2 <= 3        
         
         """
         intervals.sort()
@@ -20,15 +18,10 @@ class Solution:
             if max_start <= min_end:                
                 intervals[i+1] = [min(intervals[i][0], intervals[i+1][0]), max(intervals[i][1], intervals[i+1][1])]
                 del intervals[i]
-                # print(intervals)
-                # merged.append()
                 
             else:
-                # merged.append(intervals[i])
                 i += 1
-        
-        # if i < len(intervals):
-        #     merged.append(intervals[i])
+
         return intervals
                 
             
