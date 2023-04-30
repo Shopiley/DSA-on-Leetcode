0eb5c92cd815efb5f323c3c1f90076f2de2b4881
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-04-30 13:43:23 +0100

    Time: 262 ms (48.02%), Space: 29 MB (5.45%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -3,23 +3,17 @@ class Solution:
 
         left = 0
         right = 0
-        currentSum = nums[0]
+        currentSum = 0
         length = inf
 
         while right < len(nums):
-
-            if currentSum >= target:
+            currentSum = currentSum + nums[right]
+            while currentSum >= target:
                 length = min(length, right-left+1)
-
-                if length == 1:
-                    right += 1
                 currentSum -= nums[left]
-                left += 1               
-                continue
-
+                left += 1
+            
             right += 1
-            if right < len(nums):
-                currentSum = currentSum + nums[right]
 
         return 0 if length==inf else length
         
