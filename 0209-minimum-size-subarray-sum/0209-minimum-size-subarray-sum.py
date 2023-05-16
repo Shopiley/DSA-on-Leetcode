dfa622128f1b040e398b6dae50f4d7b7bf74ff54
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-05-16 10:37:38 +0100

    Time: 630 ms (5.04%), Space: 29 MB (8.78%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,5 +1,27 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
+        left = 0
+        right = 0
+        currentSum = 0
+        length = inf
+
+        while right < len(nums):
+            currentSum = currentSum + nums[right]
+            while currentSum >= target:
+                length = min(length, right-left+1)
+                currentSum -= nums[left]
+                left += 1
+            
+            right += 1
+
+        return 0 if length==inf else length
+    
+    
+    
+# O(n log(n))
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         def helper(length):
             s, l = 0, 0
             for r in range(len(nums)):
