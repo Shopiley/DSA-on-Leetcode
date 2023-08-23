94ffc5c717fe303094ce71c383b19d74ceabfd1d
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-08-23 08:07:09 +0100

    Time: 106 ms (64.95%), Space: 21.1 MB (68.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # group into hash map
         hashMap = {}
         for num in nums:
             hashMap[num] = 1 + hashMap.get(num, 0)
         
-        sortedMap = sorted(hashMap.items(), key=lambda x:x[1], reverse=True)
+        # sort hash map by key, produces a list of (key, value) pair in sets
+        sortedMap = sorted(hashMap.items(), key=lambda x:x[1]) #[(1, 3), (2, 2), (3, 1)]
         
         # just trying smth out
-        print(sortedMap)
         print(dict(sortedMap))
         
+        #loop through to pick top k
         ans = []
         for i in range(k):
-            ans.append(sortedMap[i][0])
+            ans.append(sortedMap[::-1][i][0])
         
         return ans
             
