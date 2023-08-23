0e493b4effabb42cddd7ab515dde9fa776504915
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-08-23 07:51:25 +0100

    Time: 99 ms (81.44%), Space: 21.1 MB (50.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,6 +6,10 @@ class Solution:
         
         sortedMap = sorted(hashMap.items(), key=lambda x:x[1], reverse=True)
         
+        # just trying smth out
+        print(sortedMap)
+        print(dict(sortedMap))
+        
         ans = []
         for i in range(k):
             ans.append(sortedMap[i][0])
