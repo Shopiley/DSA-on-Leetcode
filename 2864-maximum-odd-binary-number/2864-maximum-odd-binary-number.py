56afc76c1925c3b089a42fc7252e775289bac1fa
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2024-03-01 18:29:50 +0100

    Time: 45 ms (18.53%), Space: 16.6 MB (75.75%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -15,30 +15,33 @@ class Solution:
         
         1110001
         """
-        if s.count('1') == 0 or s.count('0') == 0:
+        ones = s.count('1')
+        zeroes = s.count('0') 
+        if ones == 0 or zeroes == 0:
             return s
         
         s = list(s)
         ans = [0 for i in range(len(s))] #[1110001]
-        freq_map = {}
+        # freq_map = {}
         
-        for c in s:
-            if c not in freq_map: #{0:0, 1:0}
-                freq_map[c] = 0
-            freq_map[c] += 1
+        # for c in s:
+        #     if c not in freq_map: #{0:0, 1:0}
+        #         freq_map[c] = 0
+        #     freq_map[c] += 1
         
         ans[-1] = '1'
-        freq_map['1'] -= 1
+        # freq_map['1'] -= 1
+        ones -= 1
         idx = 0
             
-        while freq_map['1'] > 0:
+        while ones > 0:
             ans[idx] = '1'
-            freq_map['1'] -= 1
+            ones -= 1
             idx += 1
             
-        while freq_map['0'] > 0:
+        while zeroes > 0:
             ans[idx] = '0'
-            freq_map['0'] -= 1
+            zeroes -= 1
             idx += 1
             
         return ''.join(ans)
